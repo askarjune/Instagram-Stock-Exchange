2181d5c2ef0541fe732a3281e26d33b0d125442b
Amber Skarjune <askarjun@wellesley.edu>
2017-02-26 09:46:17 -0500

    kinda bla

--- user_market.js
+++ user_market.js
@@ -6,78 +6,96 @@ PURPOSE: Hack@Smith Hackathon
 
 //User Side Implementation
 var userCoin;
-var userInstas
-var fs = require('fs');
+var userQuantity;
+var userNum = {};
 
 userCoin = 100;
 file = "yeehaw_memes.json"
-userQuantity = {"therock" : 5}
+userQuantity = {};
+
+
 
 //Market Side Implementation
-var marketInstas
+var marketInstas = {"therock": {"current": 26, "20values": [810, 829, 836, 847, 858, 872, 884, 909, 920, 948, 967, 969, 984, 1, 5, 27, 51, 66, 85, 26], "growth": -59, "previous": 85}, "choi_seung_hyun_tttop": {"current": 522, "20values": [476, 479, 485, 484, 487, 487, 486, 487, 491, 492, 496, 495, 498, 504, 505, 509, 514, 516, 516, 522], "growth": 6, "previous": 516}, "katyperry": {"current": 427, "20values": [378, 382, 393, 389, 391, 84, 388, 385, 387, 396, 396, 396, 405, 401, 399, 400, 411, 416, 428, 427], "growth": -1, "previous": 428}, "beyonce": {"current": 533, "20values": [347, 360, 64, 378, 399, 411, 421, 412, 412, 419, 433, 440, 41, 447, 470, 470, 496, 502, 518, 533], "growth": 15, "previous": 518}};
+
 
 //Functions
 
-function sellHashtag(tag){
-    var profit = marketValue[tag];
+function sellHashtag(){
+	
+	var buttonNum = event.target.id;
+	var tag;
+	
+	if (buttonNum = "b1"){tag = 1;}
+	else if (buttonNum ="b2"){tag = 2;}
+	else if (buttonNum ="b3"){tag = 3;}
+	else if (buttonNum ="b4"){tag = 4;}
+	else if (buttonNum ="b5"){tag = 5;}
+
+
+	var keys = Object.keys(marketInstas);
+	var celeb = keys[tag];
+	
+	
+    var profit = marketInstas[celeb]["current"];
     userCoin += profit;
+	
     //removes sold hashtags from userQuantity
-    userQuantity[tag] -= 1;
+    delete userQuantity[celeb];
   
   //removes hashtag from userQuantity if user has none
-  if (userQuantity[tag] <= 0){
+  if (userQuantity[celeb] <= 0){
     delete userQuantity[tag];
   }
 
-  for (i = 0; i < userHashtag.length(); i++){
-    if (userHashtag[i] == tag){
-      array.splice(i, 1);
-    }
-  }
 
 }
 
 
-function buyHashtag(tag){
-  var profit = marketValue[tag];
+function buyHashtag(){
+	
+	var buttonNum = event.target.id;
+	var tag;
+	
+	if (buttonNum = "b1"){tag = 1;}
+	else if (buttonNum = "b2"){tag = 2;}
+	else if (buttonNum = "b3"){tag = 3;}
+	else if (buttonNum = "b4"){tag = 4;}
+	else if (buttonNum = "b5"){tag = 5;}
+	else if (buttonNum = "b6"){tag = 6;}
+	else if (buttonNum = "b7"){tag = 7;}
+	else if (buttonNum = "b8"){tag = 8;}
+	else if (buttonNum = "b9"){tag = 9;}
+	else if (buttonNum = "b10"){tag = 10;}
+	
+  var profit = marketInstas[tag]["current"];
   userCoin -= profit;
-
+  var count;
+  
+  var keys = Object.keys(marketInstas);
+  var celeb = keys[tag];
+  
   //adds bought hashtags to userQuantity
-  for (i = 0; i < userHashtags.length(); i++){
-    if (userQuantity[i] == tag){
-      userQuantity[i] += 1;
-    }
-    else{
-      dict.push({
-        key: tag,
-        value: 1
-      });
-    }
-  }
+  for (key in userQuantity){
+	count++;
+   }
+   
+   if (count > 5){
+	   alert("You already own the max instas you can have!");
+   }
+	else{
+		userQuantity.push({
+			key:   celeb[tag],
+			value: marketInstas[tag]
+		});
+		
+		userNum.push({
+			key:celeb[tag],
+			value: 1
+			
+		})
+	}
+ 
 }
 
-function getMarkets() {
-  return marketHashtags;
-}
-
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-    return allText;
-}
-
-setInterval(readTextFile, 10000);
 
